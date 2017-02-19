342bf100bf9e9f8f5ed7dd83290d054db207c9bb
Gage Phillips <Gagerdude@gmail.com>
2017-02-19 00:12:57 -0600

    Fixed JS file for Trent

--- src/c/StringSplitting.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRINGSPLITTING_H
-#define STRINGSPLITTING_H
-
-char *strdup (const char *s);
-char ** str_split(char* a_str, const char* a_delim, int eateries); 
-char * strtok_r(char *s, const char *delim, char **lasts);
-#endif
